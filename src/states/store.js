7516f8eebd77c79710e65a66f2fef7280d950fa3
Ricky Ariansyah <rickyarians@outlook.com>
2021-03-27 14:11:19 +0700

    update todo

--- src/states/store.js
+++ src/states/store.js
@@ -1,6 +1,7 @@
 import { combineReducers, createStore } from "redux";
+import todos from "./todo/reducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ todos });
 
 const store = createStore(rootReducer);
 
